b118ff2175a6ee7e96dd00a3b4a68326b2ecada1
umair.malik <umair.malik@purelogics.net>
2017-08-25 17:56:57 +0500

    subscriber email

--- app/Http/Controllers/Frontend/SubscriptionController.php
+++ app/Http/Controllers/Frontend/SubscriptionController.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\Redirect;
 use App\Http\Controllers\Controller;
 use App\SubscriptionModel;
 use App\Http\Requests\SubscriptionRequest;
+use Mail;
 
 class SubscriptionController extends Controller {
 
@@ -30,14 +31,28 @@ class SubscriptionController extends Controller {
         );
         $alreadySubscribe = SubscriptionModel::CheckSubscription($data);
         if (!$alreadySubscribe) {
-            $message = 'Successfully Subscribed To Our Channel!';
+            $message = 'Successfully Subscribed To Our Channel! Please Check Your Email.';
             SubscriptionModel::SaveSubscription($data);
         } else {
             $message = 'Already Subscribed To Our Channel!';
         }
-        
+
         \Session::flash('message', $message);
+        //send email to user
+        $EmailSubject = "Subscription Request";
+        $this->SendEmail($userEmail, $EmailSubject, 'emails.subscriptionApplicationEmail');
         return Redirect::back()->withErrors(['message', $message]);
+    }
+
+    public function SendEmail($EmailTo, $Subject, $body) {
+        $data = array('email' => $EmailTo, 'subject' => $Subject);
+        $result = Mail::send($body, $data, function ($message) use ($data) {
+                    $message->from('umair.malik@purelogics.net', 'Youth Media');
+                    $message->to($EmailTo);
+                    $message->replyTo('umair.malik@purelogics.net', 'Youth Media');
+                    $message->subject($data['subject']);
+                });
+        return $result;
     }
 
 }
