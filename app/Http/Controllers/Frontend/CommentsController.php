d9557353e9dd164f4aa0fff91210268608432c5d
umair.malik <umair.malik@purelogics.net>
2017-08-30 16:23:25 +0500

    new comment notification

--- app/Http/Controllers/Frontend/CommentsController.php
+++ app/Http/Controllers/Frontend/CommentsController.php
@@ -7,7 +7,10 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Redirect;
 use App\Http\Controllers\Controller;
 use App\CommentModel;
+use App\PostsModel;
+use App\UserModel;
 use App\Http\Requests\CommentRequest;
+use Mail;
 
 class CommentsController extends Controller {
 
@@ -29,8 +32,32 @@ class CommentsController extends Controller {
         $message = 'Comment Added!';
         CommentModel::SaveComment($data);
 
+        //send email to user
+        $post = PostsModel::GetSinglePostDetail($data);
+        if ($post[0]->userId != $userId) {
+            $userData = array('id' => $userId);
+            $user = UserModel::GetSingleUser($userData);
+            if ($user[0]->email != '') {
+                $EmailSubject = "New Comment Received!";
+                $videoData = array(
+                    'title' => $post[0]->postTitle,
+                    'url' => url('video/'.base64_encode($post[0]->postId))
+                );
+                $this->SendEmail($user[0]->email, $EmailSubject, 'emails.commentEmail', $videoData);
+            }
+        }
         \Session::flash('message', $message);
         return Redirect::back()->withErrors(['message', $message]);
+    }
+
+    public function SendEmail($EmailTo, $Subject, $body, $data) {
+        $data = array('data' => $data, 'email' => $EmailTo, 'subject' => $Subject);
+        $result = Mail::send($body, $data, function ($message) use ($data) {
+                    $message->from('umair.malik@purelogics.net', 'Youth Media');
+                    $message->to($data['email']);
+                    $message->subject($data['subject']);
+                });
+        return $result;
     }
 
 }
