88c24e0ced9868857b99172c0f89ecfaacae63e7
Rashid <rashid.bukhari78600@gmail.com>
2019-01-02 17:47:22 +0500

    scrape zemtv and save data into database route save/zemtv

--- public/scrapping/libraries/dbConfig.php
+++ public/scrapping/libraries/dbConfig.php
@@ -1,6 +1,6 @@
 <?php
 
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -15,12 +15,12 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "youthmedia";
+$dbname = "youtube_test";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password , $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname);
 
 // Check connection
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
+	die("Connection failed: " . $conn->connect_error);
+}
--- routes/web.php
+++ routes/web.php
@@ -1,90 +1,91 @@
 <?php
 
 /*
-  |--------------------------------------------------------------------------
-  | Web Routes
-  |--------------------------------------------------------------------------
-  |
-  | Here is where you can register web routes for your application. These
-  | routes are loaded by the RouteServiceProvider within a group which
-  | contains the "web" middleware group. Now create something great!
-  |
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
  */
+Route::get('save/zemtv', ['as' => 'zemtv', 'uses' => 'TestController@index']);
 
 Route::group(['middleware' => ['web', 'auth', 'admin']], function () {
-    //route for admin
-    Route::get('admin', ['as' => 'admin', 'uses' => 'AdminHomeController@index']);
+	//route for admin
+	Route::get('admin', ['as' => 'admin', 'uses' => 'AdminHomeController@index']);
 
-    Route::post('settings', ['as' => 'settings', 'uses' => 'AdminHomeController@settings']);
-    Route::post('deleteSocial', ['as' => 'deleteSocial', 'uses' => 'AdminHomeController@deleteSocial']);
-    Route::post('getSocial', ['as' => 'getSocial', 'uses' => 'AdminHomeController@getSocial']);
-    Route::post('updateSocial', ['as' => 'updateSocial', 'uses' => 'AdminHomeController@updateSocial']);
-    //route for user settings
-    Route::get('profile', ['as' => 'profile', 'uses' => 'UserController@profile']);
-    Route::get('userList', ['as' => 'userList', 'uses' => 'UserController@userList']);
-    Route::get('addUser', ['as' => 'addUser', 'uses' => 'UserController@addUser']);
-    Route::get('editUser/{userId}', ['as' => 'editUser', 'uses' => 'UserController@profile']);
-    Route::get('deleteUser/{userId}', ['as' => 'deleteUser', 'uses' => 'UserController@deleteUser']);
+	Route::post('settings', ['as' => 'settings', 'uses' => 'AdminHomeController@settings']);
+	Route::post('deleteSocial', ['as' => 'deleteSocial', 'uses' => 'AdminHomeController@deleteSocial']);
+	Route::post('getSocial', ['as' => 'getSocial', 'uses' => 'AdminHomeController@getSocial']);
+	Route::post('updateSocial', ['as' => 'updateSocial', 'uses' => 'AdminHomeController@updateSocial']);
+	//route for user settings
+	Route::get('profile', ['as' => 'profile', 'uses' => 'UserController@profile']);
+	Route::get('userList', ['as' => 'userList', 'uses' => 'UserController@userList']);
+	Route::get('addUser', ['as' => 'addUser', 'uses' => 'UserController@addUser']);
+	Route::get('editUser/{userId}', ['as' => 'editUser', 'uses' => 'UserController@profile']);
+	Route::get('deleteUser/{userId}', ['as' => 'deleteUser', 'uses' => 'UserController@deleteUser']);
 
-    Route::post('updateProfile', ['as' => 'updateProfile', 'uses' => 'UserController@updateProfile']);
-    Route::post('insertUser', ['as' => 'insertUser', 'uses' => 'UserController@insertUser']);
-    //route for permissions
-    Route::get('permissions', ['as' => 'permissions', 'uses' => 'PermissionController@index']);
-    Route::get('roles', ['as' => 'manageRoles', 'uses' => 'PermissionController@manageRoles']);
-    Route::get('deleteRole/{roleId}', ['as' => 'deleteRole', 'uses' => 'PermissionController@deleteRole']);
+	Route::post('updateProfile', ['as' => 'updateProfile', 'uses' => 'UserController@updateProfile']);
+	Route::post('insertUser', ['as' => 'insertUser', 'uses' => 'UserController@insertUser']);
+	//route for permissions
+	Route::get('permissions', ['as' => 'permissions', 'uses' => 'PermissionController@index']);
+	Route::get('roles', ['as' => 'manageRoles', 'uses' => 'PermissionController@manageRoles']);
+	Route::get('deleteRole/{roleId}', ['as' => 'deleteRole', 'uses' => 'PermissionController@deleteRole']);
 
-    Route::post('setModulePermissions', ['as' => 'set-module-permissions', 'uses' => 'PermissionController@setModulePermissions']);
-    Route::post('saveRole', ['as' => 'saveRole', 'uses' => 'PermissionController@saveRole']);
-    Route::post('getRole', ['as' => 'getRole', 'uses' => 'PermissionController@getRole']);
-    Route::post('updateRole', ['as' => 'updateRole', 'uses' => 'PermissionController@updateRole']);
-    //route for pages
-    Route::get('pages', ['as' => 'pages', 'uses' => 'PageController@index']);
-    Route::get('editPage/{pageId}', ['as' => 'editPage', 'uses' => 'PageController@editPage']);
+	Route::post('setModulePermissions', ['as' => 'set-module-permissions', 'uses' => 'PermissionController@setModulePermissions']);
+	Route::post('saveRole', ['as' => 'saveRole', 'uses' => 'PermissionController@saveRole']);
+	Route::post('getRole', ['as' => 'getRole', 'uses' => 'PermissionController@getRole']);
+	Route::post('updateRole', ['as' => 'updateRole', 'uses' => 'PermissionController@updateRole']);
+	//route for pages
+	Route::get('pages', ['as' => 'pages', 'uses' => 'PageController@index']);
+	Route::get('editPage/{pageId}', ['as' => 'editPage', 'uses' => 'PageController@editPage']);
 
-    Route::post('setPageSettings', ['as' => 'setPageSettings', 'uses' => 'PageController@setPageSettings']);
-    Route::post('deleteAfterParty', ['as' => 'deleteAfterParty', 'uses' => 'AdminAfterPartyController@deleteAfterParty']);
-    //route for website settings
-    Route::get('websites', ['as' => 'websites', 'uses' => 'WebsitesController@index']);
-    Route::get('addWebsite', ['as' => 'addWebsite', 'uses' => 'WebsitesController@addWebsite']);
-    Route::get('editWebsite/{websiteId}', ['as' => 'editWebsite', 'uses' => 'WebsitesController@getWebsite']);
+	Route::post('setPageSettings', ['as' => 'setPageSettings', 'uses' => 'PageController@setPageSettings']);
+	Route::post('deleteAfterParty', ['as' => 'deleteAfterParty', 'uses' => 'AdminAfterPartyController@deleteAfterParty']);
+	//route for website settings
+	Route::get('websites', ['as' => 'websites', 'uses' => 'WebsitesController@index']);
+	Route::get('addWebsite', ['as' => 'addWebsite', 'uses' => 'WebsitesController@addWebsite']);
+	Route::get('editWebsite/{websiteId}', ['as' => 'editWebsite', 'uses' => 'WebsitesController@getWebsite']);
 
-    Route::post('insertWebsite', ['as' => 'insertWebsite', 'uses' => 'WebsitesController@insertWebsite']);
-    Route::post('deleteWebsite', ['as' => 'deleteWebsite', 'uses' => 'WebsitesController@deleteWebsite']);
-    //route for categories settings
-    Route::get('categories', ['as' => 'categories', 'uses' => 'CategoriesController@index']);
-    Route::get('addCategory', ['as' => 'addCategory', 'uses' => 'CategoriesController@addCategory']);
-    Route::get('editCategory/{categoryId}', ['as' => 'editCategory', 'uses' => 'CategoriesController@getCategory']);
+	Route::post('insertWebsite', ['as' => 'insertWebsite', 'uses' => 'WebsitesController@insertWebsite']);
+	Route::post('deleteWebsite', ['as' => 'deleteWebsite', 'uses' => 'WebsitesController@deleteWebsite']);
+	//route for categories settings
+	Route::get('categories', ['as' => 'categories', 'uses' => 'CategoriesController@index']);
+	Route::get('addCategory', ['as' => 'addCategory', 'uses' => 'CategoriesController@addCategory']);
+	Route::get('editCategory/{categoryId}', ['as' => 'editCategory', 'uses' => 'CategoriesController@getCategory']);
 
-    Route::post('insertCategory', ['as' => 'insertCategory', 'uses' => 'CategoriesController@insertCategory']);
-    Route::post('deleteCategory', ['as' => 'deleteCategory', 'uses' => 'CategoriesController@deleteCategory']);
-    //route for post settings
-    Route::get('posts', ['as' => 'posts', 'uses' => 'PostsController@index']);
-    Route::get('addPost', ['as' => 'addPost', 'uses' => 'PostsController@addPost']);
-    Route::get('editPost/{postId}', ['as' => 'editPost', 'uses' => 'PostsController@getPost']);
-    Route::get('pending', ['as' => 'pending', 'uses' => 'PostsController@pending']);
-    Route::get('createThumb/{postId}/{videoId}', ['as' => 'createThumb', 'uses' => 'PostsController@createThumb']);
+	Route::post('insertCategory', ['as' => 'insertCategory', 'uses' => 'CategoriesController@insertCategory']);
+	Route::post('deleteCategory', ['as' => 'deleteCategory', 'uses' => 'CategoriesController@deleteCategory']);
+	//route for post settings
+	Route::get('posts', ['as' => 'posts', 'uses' => 'PostsController@index']);
+	Route::get('addPost', ['as' => 'addPost', 'uses' => 'PostsController@addPost']);
+	Route::get('editPost/{postId}', ['as' => 'editPost', 'uses' => 'PostsController@getPost']);
+	Route::get('pending', ['as' => 'pending', 'uses' => 'PostsController@pending']);
+	Route::get('createThumb/{postId}/{videoId}', ['as' => 'createThumb', 'uses' => 'PostsController@createThumb']);
 
-    Route::post('insertPost', ['as' => 'insertPost', 'uses' => 'PostsController@insertPost']);
-    Route::post('deletePost', ['as' => 'deletePost', 'uses' => 'PostsController@deletePost']);
-    //route for subscription settings
-    Route::get('usersubscription', ['as' => 'usersubscription', 'uses' => 'SubscriptionController@index']);
-    Route::get('addSubscriber', ['as' => 'addSubscriber', 'uses' => 'SubscriptionController@addSubscriber']);
-    Route::get('editSubscriber/{subscriptionId}', ['as' => 'editSubscriber', 'uses' => 'SubscriptionController@getSubscriber']);
-    Route::get('deleteSubscriber/{subscriptionId}', ['as' => 'deleteSubscriber', 'uses' => 'SubscriptionController@deleteSubscriber']);
-    
-    Route::post('insertSubscriber', ['as' => 'insertSubscriber', 'uses' => 'SubscriptionController@insertSubscriber']);
-    //route for image upload via tinymce
-    Route::post('postAcceptor', ['as' => 'postAcceptor', 'uses' => 'GalleryController@postAcceptor']);
+	Route::post('insertPost', ['as' => 'insertPost', 'uses' => 'PostsController@insertPost']);
+	Route::post('deletePost', ['as' => 'deletePost', 'uses' => 'PostsController@deletePost']);
+	//route for subscription settings
+	Route::get('usersubscription', ['as' => 'usersubscription', 'uses' => 'SubscriptionController@index']);
+	Route::get('addSubscriber', ['as' => 'addSubscriber', 'uses' => 'SubscriptionController@addSubscriber']);
+	Route::get('editSubscriber/{subscriptionId}', ['as' => 'editSubscriber', 'uses' => 'SubscriptionController@getSubscriber']);
+	Route::get('deleteSubscriber/{subscriptionId}', ['as' => 'deleteSubscriber', 'uses' => 'SubscriptionController@deleteSubscriber']);
+
+	Route::post('insertSubscriber', ['as' => 'insertSubscriber', 'uses' => 'SubscriptionController@insertSubscriber']);
+	//route for image upload via tinymce
+	Route::post('postAcceptor', ['as' => 'postAcceptor', 'uses' => 'GalleryController@postAcceptor']);
 });
 Route::group(['middleware' => ['web', 'auth']], function () {
-    //route for profile settings
-    Route::get('/settings', ['as' => 'settings', 'uses' => 'Frontend\UserController@index']);
-    
-    Route::post('userSettings', ['as' => 'userSettings', 'uses' => 'Frontend\UserController@userSettings']);
-    //route for comments
-    Route::post('postComment', ['as' => 'postComment', 'uses' => 'Frontend\CommentsController@postComment']);
-    //route for deleting post by both admin or frontend side
-    Route::post('deleteUserPost', ['as' => 'deleteUserPost', 'uses' => 'Frontend\PostsController@deleteUserPost']);
+	//route for profile settings
+	Route::get('/settings', ['as' => 'settings', 'uses' => 'Frontend\UserController@index']);
+
+	Route::post('userSettings', ['as' => 'userSettings', 'uses' => 'Frontend\UserController@userSettings']);
+	//route for comments
+	Route::post('postComment', ['as' => 'postComment', 'uses' => 'Frontend\CommentsController@postComment']);
+	//route for deleting post by both admin or frontend side
+	Route::post('deleteUserPost', ['as' => 'deleteUserPost', 'uses' => 'Frontend\PostsController@deleteUserPost']);
 });
 Auth::routes();
 //routes for frontend
